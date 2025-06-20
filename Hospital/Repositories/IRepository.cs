1db8cf14b46aec246d2106d3b08490a5a3843b0b
Laura Isel <laura.isel@gmail.com>
2025-06-20 18:24:05 +0200

    Added repository factory pattern

---
+++ Hospital/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Hospital.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        Task SaveAsync();
+    }
+
+}

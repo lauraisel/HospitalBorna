6176f3243b69a9e5ff2eea20e4f2b42331837ac9
Laura Isel <laura.isel@gmail.com>
2025-06-20 18:06:19 +0200

    Added models and db connection

--- Hospital/Program.cs
+++ Hospital/Program.cs
@@ -1,3 +1,7 @@
+using Hospital;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,27 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+        .UseLazyLoadingProxies());
+
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+    if (dbContext.Database.CanConnect())
+    {
+        Console.WriteLine("Successfully connected to the database.");
+    }
+    else
+    {
+        Console.WriteLine("Failed to connect to the database.");
+    }
+}
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
